14ca189b31e79c4648e559c9253d7412428e36a2
Jaisheesh-2006 <jaicodes2006@gmail.com>
2025-12-27 14:01:39 +0530

    Server setup

--- cmd/students-api/main.go
+++ cmd/students-api/main.go
@@ -1,7 +1,38 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+
+	"github.com/Jaisheesh-2006/Golang-rest-api/internal/config"
+	"github.com/fatih/color"
+)
 
 func main() {
-	fmt.Println("Hello, Students API!")
+	//* load configuration
+
+	cfg := config.MustLoad()
+
+	//* database setup
+
+	//* setup router
+
+	router := http.NewServeMux()
+	router.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Welcome to students api"))
+	})
+
+	//* start server
+
+	server := http.Server{
+		Addr:    cfg.Address,
+		Handler: router,
+	}
+
+	color.Green("Server started at %s", cfg.Address)
+	err := server.ListenAndServe()
+	if err != nil {
+		log.Fatalf("Error while starting the server")
+	}
+
 }
