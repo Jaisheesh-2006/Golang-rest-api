85602182ff81b0a163b65ca4f36d031312af2ac7
Jaisheesh-2006 <jaicodes2006@gmail.com>
2025-12-27 15:58:57 +0530

    Graceful shutdown

--- cmd/students-api/main.go
+++ cmd/students-api/main.go
@@ -1,8 +1,14 @@
 package main
 
 import (
+	"context"
 	"log"
+	"log/slog"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/Jaisheesh-2006/Golang-rest-api/internal/config"
 	"github.com/fatih/color"
@@ -30,9 +36,31 @@ func main() {
 	}
 
 	color.Green("Server started at %s", cfg.Address)
-	err := server.ListenAndServe()
-	if err != nil {
-		log.Fatalf("Error while starting the server")
+
+	done := make(chan os.Signal, 1)
+
+	signal.Notify(done, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+
+	go func() {
+		err := server.ListenAndServe()
+		if err != nil {
+			log.Fatalf("Error while starting the server")
+		}
+	}()
+
+	//* Until we receive signal keep the server running
+	<-done
+
+	//* Graceful shutdown
+
+	slog.Info("shutting down the server")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	if err := server.Shutdown(ctx); err != nil {
+		slog.Error("error while shutting down the server", slog.String("error", err.Error()))
 	}
+	slog.Info("server stopped successfully")
 
 }
